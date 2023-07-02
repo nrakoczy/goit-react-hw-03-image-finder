4d7777e7a370c6d2c4a09eab30db9b5b7288a16c
NataliaRakoczy <natalia.rakoczy54@gmail.com>
2023-07-02 12:23:02 +0200

    added code

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './Modal.module.css';
+
+class Modal extends Component {
+  static propTypes = {
+    closeModal: PropTypes.func.isRequired,
+    lgImage: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    const { lgImage, tags } = this.props;
+    return (
+      <div className={style.Overlay} onClick={this.handleBackdropClick}>
+        <div className={style.Modal}>
+          <img src={lgImage} alt={tags} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
---
+++ src/components/services/settings.js
@@ -0,0 +1,9 @@
+
+const settings = {
+  BASE_URL: 'https://pixabay.com/api/?',
+  API_KEY: 'key=36389818-5d3972b65ac518b4ed94f04af',
+  TYPE: '&image_type=photo',
+  PER_PAGE: '&per_page=12',
+};
+
+export default settings;
